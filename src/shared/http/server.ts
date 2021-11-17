af0ebbb7eaf6a420956a0d47d77e119da24c62f6
Nicholas Velten <nikvelten@gmail.com>
2021-11-17 15:07:18 -0300

    Initial commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Listening on port 3333');
+});
